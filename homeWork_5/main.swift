03e03a5b9caf46544e381acebf371819c8918bb9
ayzsw <121678677+ayzsw@users.noreply.github.com>
2023-01-18 23:25:17 +0600

    Update main.swift

--- homeWork_5/main.swift
+++ homeWork_5/main.swift
@@ -1,4 +1,3 @@
-
 import Foundation
 
 print("------Чек-------")
@@ -10,4 +9,20 @@ basket["Cola"] = 100
 for (nameBasket, price) in basket{
     print("\(nameBasket)              \(price) com")
 }
+
+
+
 print("-----------------\nItog - \(basket["Bread"]! + basket["Milk"]! + basket["Cola"]!) com")
+print("Что из этого хотите удалить?")
+var korzina = readLine()!
+
+
+if korzina == "Bread"{
+        print("Bread - в корзине, теперь  ваш чек - 170 сом")
+}else if korzina == "Milk"{
+    print("Milk - в корзине, теперь  ваш чек - 125 сом")
+}else if korzina == "Cola"{
+    print("Cola - в корзине, теперь  ваш чек - 95 сом")
+}else{
+    print("Такого товара нет)")
+}
